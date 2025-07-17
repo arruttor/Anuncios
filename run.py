3f7e76acee65e33125dd6ea7d226aa67a6f0c711
VINICIUS MARTINS BORGES <vinicius.m.borges@una.br>
2025-07-17 13:17:32 -0300

    tentando colocar gevent

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, server='gevent')
+    socketio.run(app, host="0.0.0.0", port=port)
